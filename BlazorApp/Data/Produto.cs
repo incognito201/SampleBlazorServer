930ecc3b2cb8d86583032f640472b1346cca1963
incognito201 <99709164+incognito201@users.noreply.github.com>
2023-04-15 16:40:30 +0000

    Propriedade Nullable desabilitada

--- BlazorApp/Data/Produto.cs
+++ BlazorApp/Data/Produto.cs
@@ -4,9 +4,9 @@ public class Produto
 {
     public int Id { get; set; }
 
-    public string Nome { get; set; } = "";
+    public string Nome { get; set; }
 
     public double Preco { get; set; }
 
-    public string Categoria { get; set; } = "";
+    public string Categoria { get; set; }
 }
